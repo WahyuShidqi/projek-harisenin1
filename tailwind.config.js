36c3509104d47e9c622cbf2e6e1dfe42d1551618
Wahyu Shidqi <wahyushidqi550@gmail.com>
2023-03-31 10:13:50 +0700

    commit pertama

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      keyframes: {
+        dada: {
+          "0%": { opacity: "0", },
+          "2%": { opacity: "0", transform: "translateY(-30px)"},
+          "5%": { opacity: "1", transform: "translateY(0px)" },
+          "17%": { opacity: "1", transform: "translateY(0px)" },
+          "20%": { opacity: "0", transform: "translateY(30px)" },
+        }
+      },
+      animation: {
+        dada: "dada 10s linear infinite 0s",
+      }
+    },
+  },
+  plugins: [],
+}
+
